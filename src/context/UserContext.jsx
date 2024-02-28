90abfe8edc616b9ef47e96f54a61dc2e63fac1fd
JPS4L4 <juansalabet1@gmail.com>
2024-02-27 22:50:21 -0500

    dia 1

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,15 +1,28 @@
-import { useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { createContext } from "react";
+import { auth } from "../config/firebase";
 
 const UserContext = createContext();
 
 export default function UserContextProvider({ children }) {
-    const [user, setUser] = useState(false);
+  const [user, setUser] = useState(false);
 
-    return (
-        <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
-    );
+  useEffect(() => {
+    console.log("Use effect en acción");
+    const unsuscribe = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      setUser(user);
+    });
+    return unsuscribe;
+  }, []);
+
+  if (user === false) return <p>Loading App...</p>;
+
+  return (
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+  );
 }
 
 export const useUserContext = () => useContext(UserContext);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import { login } from "../config/firebase";
+import { useUserContext } from "../context/UserContext";
+import { useRedirectActiveUser } from "../hooks/useRedirectActiveUse";
+
 const Login = () => {
-    return "Login";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {user} = useUserContext();
+  useRedirectActiveUser(user, "/dashboard")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const credentialUser = await login({ email, password });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ingrese Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Ingrese Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
